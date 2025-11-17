816998ee043dce153972068301b94b618d1f77ac
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-11-17 12:34:26 +0100

    Validacion de datos de formularios

--- presentation/controllers/auth_controller.py
+++ presentation/controllers/auth_controller.py
@@ -8,5 +8,9 @@ def auth():
     if request.method == 'POST':
         name = request.form.get('username')
         password = request.form.get('password')
-        print(f'{name} - {password}')
+        if len(name)>=4 and len(name)<=6 and len(password)>=6 and len(password)<=40:
+            print(f'{name} - {password}')
+        else:
+            error ="""El nombre de usuario debe tener entre 4 y 25 caractyres y la contraseña entre 6 y 40 caracteres"""
+            return render_template('auth/register.html', error=error)
     return render_template('auth/register.html')
