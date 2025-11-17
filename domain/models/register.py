c26b59c166f873858f7add247bfc924791aebed2
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-11-17 13:11:47 +0100

    Validacion de datos con WTForm

--- domain/models/register.py
+++ domain/models/register.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms.fields.simple import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 
 class RegisterFormWtf(FlaskForm):
-    username = StringField('Nombre de usuario:')
-    password = PasswordField('Contraseña:')
+    username = StringField('Nombre de usuario:',validators=[DataRequired(), Length(min=4, max=25)])
+    password = PasswordField('Contraseña:', validators=[DataRequired(), Length(min=6, max=40)])
     submit = SubmitField('Registrar')
